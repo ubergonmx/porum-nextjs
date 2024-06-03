b0ed65aedbad21ea70acee76a29b9361e5231e95
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-06-03 23:59:47 +0800

    Add phoneNumber and profileImage columns to users table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,11 +1,13 @@
 import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
-export const users = pgTable("bb_user", {
+export const users = pgTable("p_user", {
   id: text("id")
     .primaryKey()
     .$defaultFn(() => crypto.randomUUID()),
   name: text("name"),
   email: text("email").notNull(),
   emailVerified: timestamp("emailVerified", { mode: "date" }),
+  phoneNumber: text("phoneNumber"),
+  profileImage: text("profileImage"),
   image: text("image"),
 });
