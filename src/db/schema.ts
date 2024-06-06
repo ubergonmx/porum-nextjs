fad217d213536e2e921fb495ed2468cf4af430d4
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-06-06 23:59:39 +0800

    Add cSpell configuration and update users table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,16 +1,28 @@
-import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { pgTableCreator, index, text, timestamp } from "drizzle-orm/pg-core";
+import { DATABASE_PREFIX as prefix } from "@/lib/constants";
 
-export const users = pgTable("users", {
-  id: text("id")
-    .primaryKey()
-    .$defaultFn(() => crypto.randomUUID()),
-  firstName: text("firstName").notNull(),
-  lastName: text("lastName").notNull(),
-  email: text("email").notNull(),
-  emailVerified: timestamp("emailVerified", { mode: "date" }),
-  phoneNumber: text("phoneNumber"),
-  image: text("image"),
-  role: text("role").notNull().$type<"admin" | "user">(),
-  createdAt: timestamp("createdAt", { mode: "date" }).notNull(),
-  updatedAt: timestamp("updatedAt", { mode: "date" }).notNull(),
-});
+export const pgTable = pgTableCreator((name: string) => `${prefix}_${name}`);
+
+export const users = pgTable(
+  "users",
+  {
+    id: text("id")
+      .primaryKey()
+      .$defaultFn(() => crypto.randomUUID()),
+    firstName: text("firstName").notNull(),
+    lastName: text("lastName").notNull(),
+    email: text("email").notNull(),
+    emailVerified: timestamp("emailVerified", { mode: "date" }),
+    phoneNumber: text("phoneNumber"),
+    image: text("image"),
+    role: text("role").notNull().$type<"admin" | "user">(),
+    createdAt: timestamp("createdAt", { mode: "date" }).notNull(),
+    updatedAt: timestamp("updatedAt", { mode: "date" }).notNull(),
+  },
+  (t) => ({
+    emailIdx: index("user_email_idx").on(t.email),
+  }),
+);
+
+export type User = typeof users.$inferSelect;
+export type NewUser = typeof users.$inferInsert;
---
+++ src/lib/constants.ts
@@ -0,0 +1,3 @@
+export const APP_TITLE = "Porum";
+export const DATABASE_PREFIX = "porum";
+export const EMAIL_SENDER = '"Porum" <noreply@porum.com>';
