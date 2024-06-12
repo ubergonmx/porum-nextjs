f0a1782632e11cd499c2e73c1c86b6443c004f79
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-06-12 22:26:57 +0800

    Add lucia initialization

---
+++ src/lib/auth/index.ts
@@ -0,0 +1,43 @@
+import { Lucia, TimeSpan } from "lucia";
+import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
+import { env } from "@/env";
+import { sessions, users, type User as DbUser } from "@/db/schema";
+import { database as db } from "@/db/database";
+
+const adapter = new DrizzlePostgreSQLAdapter(db, sessions, users);
+
+export const lucia = new Lucia(adapter, {
+  getSessionAttributes: () => {
+    return {};
+  },
+  getUserAttributes: (attributes) => {
+    return {
+      id: attributes.id,
+      email: attributes.email,
+      emailVerified: attributes.emailVerified,
+      avatar: attributes.avatar,
+      createdAt: attributes.createdAt,
+      updatedAt: attributes.updatedAt,
+    };
+  },
+  sessionExpiresIn: new TimeSpan(30, "d"),
+  sessionCookie: {
+    name: "session",
+    expires: false, // session cookies have very long lifespan (2 years)
+    attributes: {
+      secure: env.NODE_ENV === "production",
+    },
+  },
+});
+
+interface DatabaseSessionAttributes {}
+interface DatabaseUserAttributes extends Omit<DbUser, "password"> {}
+
+declare module "lucia" {
+  // eslint-disable-next-line no-unused-vars
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseSessionAttributes: DatabaseSessionAttributes;
+    DatabaseUserAttributes: DatabaseUserAttributes;
+  }
+}
