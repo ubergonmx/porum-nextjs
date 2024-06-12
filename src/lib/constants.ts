38fffced04e5786d4460e6146189b916831026ec
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-06-12 18:23:22 +0800

    Add paths enum

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,13 @@
 export const APP_TITLE = "Porum";
 export const DATABASE_PREFIX = "porum";
 export const EMAIL_SENDER = '"Porum" <noreply@porum.com>';
+
+/* eslint-disable no-unused-vars */
+export enum Paths {
+  Home = "/",
+  Login = "/login",
+  Signup = "/signup",
+  Dashboard = "/dashboard",
+  VerifyEmail = "/verify-email",
+  ResetPassword = "/reset-password",
+}
