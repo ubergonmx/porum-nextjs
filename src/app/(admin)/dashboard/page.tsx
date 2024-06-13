7732ef09a7781018a5eb49c1d80286c8665b3cfa
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2024-06-13 15:39:33 +0800

    Blocked non-admin users from viewing admin dashboard

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -28,8 +28,23 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import UserTable from "@/components/user-table";
 import { fetchUsers } from "./actions";
 import { User } from "@/db/schema";
+import { validateRequest } from "@/lib/auth/validate-requests";
+import { Paths } from "@/lib/constants";
+import { redirect } from "next/navigation";
 
 export default async function Dashboard() {
+  const { user } = await validateRequest();
+
+  if (user) {
+    if (user.role !== "admin") {
+      console.log("User is not an admin");
+      return redirect(Paths.Home);
+    }
+  } else {
+    console.log("User is not logged in");
+    return redirect(Paths.Login);
+  }
+
   // This loads the user database from postgres drizzle-orm
   // uses server actions to get the data using actions.ts
   const users: User[] = await fetchUsers();
