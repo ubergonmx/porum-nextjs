7f1958ffbac0ec7e53bd9c860d85a7e4d5f20731
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-06-14 04:52:59 +0800

    Fix settings page auth

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -23,8 +23,15 @@ import { Input } from "@/components/ui/input";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import AvatarUploadChunk from "./avatar-upload";
 import Image from "next/image";
+import { validateRequest } from "@/lib/auth/validate-requests";
+import { Paths } from "@/lib/constants";
+import { redirect } from "next/navigation";
 
-export default function Dashboard() {
+export default async function Settings() {
+  const { user } = await validateRequest();
+
+  if (!user) redirect(Paths.Login);
+
   return (
     <div className="flex min-h-screen w-full flex-col">
       <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
