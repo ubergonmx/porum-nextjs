4b0b220a996410999fe64554a7447027e4a28ee3
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2024-06-13 17:14:56 +0800

    Updated regex for signup

--- src/lib/validators/auth.ts
+++ src/lib/validators/auth.ts
@@ -35,15 +35,20 @@ export const signupSchema = z.object({
     .min(3, {
       message: "Email must be at least 3 characters",
     })
-    .max(255, { message: "Email must not be more than 255 characters long" })
-    .email({ message: "Invalid email address" }),
+    .max(254, { message: "Email must not be more than 254 characters long" })
+    .regex(
+      /^[a-zA-Z0-9_+&*-]+(?:\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$/, // According to OWASP
+      {
+        message: "Invalid email address",
+      },
+    ),
   password: z
     .string()
     .min(8, { message: "Password must be at least 8 characters" }),
   phone: z
     .string()
     // .refine(isValidPhoneNumber, { message: "Invalid phone number" }),
-    .regex(/^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/, {
+    .regex(/^(09|\+639)\d{9}$/, {
       message: "Invalid phone number",
     }),
 });
