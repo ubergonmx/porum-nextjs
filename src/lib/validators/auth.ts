e2e464b2545050ae664ab3c6ef6639dbf136281f
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-06-24 23:04:30 +0800

    Refactor email validation in auth validators

--- src/lib/validators/auth.ts
+++ src/lib/validators/auth.ts
@@ -4,6 +4,22 @@ import { z } from "zod";
 const MAX_FILE_SIZE = 1024 * 1024 * 5;
 const ACCEPTED_IMAGE_TYPES = ["image/png", "image/jpeg"];
 
+const EMAIL_REGEX =
+  /^(?!\.)("([^"\r\\]|\\["\r\\])*"|([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)@[a-z0-9][\w\\.-]*[a-z0-9]\.[a-z][a-z\\.]*[a-z]$/;
+// const OWASP_EMAIL_REGEX =
+//   /^[a-zA-Z0-9_+&*-]+(?:\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$/;
+
+const checkEmailLength = (email: string) => {
+  const [localPart, domainPart] = email.split("@");
+  if (!localPart || !domainPart) return false;
+
+  const getByteLength = (str: string | undefined) =>
+    new TextEncoder().encode(str).length;
+
+  // Check the byte length of the local part and the domain part
+  return getByteLength(localPart) <= 64 && getByteLength(domainPart) <= 255;
+};
+
 export const signupSchema = z.object({
   firstName: z
     .string({ required_error: "First name is required" })
@@ -47,13 +63,12 @@ export const signupSchema = z.object({
     .min(3, {
       message: "Email must be at least 3 characters",
     })
-    .max(254, { message: "Email must not be more than 254 characters long" })
-    .regex(
-      /^[a-zA-Z0-9_+&*-]+(?:\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$/, // According to OWASP
-      {
-        message: "Invalid email address",
-      },
-    ),
+    .regex(EMAIL_REGEX, {
+      message: "Invalid email address",
+    })
+    .refine(checkEmailLength, {
+      message: "Local part and domain part must be 64 and 255 bytes or less",
+    }),
   password: z
     .string()
     .min(8, { message: "Password must be at least 8 characters" }),
@@ -102,13 +117,12 @@ export const loginSchema = z.object({
     .min(3, {
       message: "Email must be at least 3 characters",
     })
-    .max(254, { message: "Email must not be more than 254 characters long" })
-    .regex(
-      /^[a-zA-Z0-9_+&*-]+(?:\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$/, // According to OWASP
-      {
-        message: "Invalid email address",
-      },
-    ),
+    .regex(EMAIL_REGEX, {
+      message: "Invalid email address",
+    })
+    .refine(checkEmailLength, {
+      message: "Local part and domain part must be 64 and 255 bytes or less",
+    }),
   password: z
     .string()
     .min(8, { message: "Password must be at least 8 characters" }),
