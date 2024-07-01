7165ce99b4bef54733049ac28e84021c375ea073
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-07-01 13:01:15 +0800

    Install @node-rs/argon2 for hashing password

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: [
+      "@node-rs/argon2",
+    ],
+  },
+};
 
 export default nextConfig;
