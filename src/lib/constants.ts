60221397ddbc83a89f09c2ebc53f0799926d744d
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-07-18 21:38:17 +0800

    Change email sender constant string

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const APP_TITLE = "Porum";
 export const DATABASE_PREFIX = "porum";
-export const EMAIL_SENDER = '"Porum" <noreply@porum.com>';
+export const EMAIL_SENDER = '"Porum" <noreply@porum.info>';
 
 /* eslint-disable no-unused-vars */
 export enum Paths {
