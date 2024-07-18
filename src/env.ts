824ea9a09759e3b8340bb3e85fb4bae958d164a3
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-07-18 20:01:03 +0800

    Fix parse int smtp port

--- src/env.ts
+++ src/env.ts
@@ -32,7 +32,7 @@ export const env = createEnv({
     UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
     UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
     SMTP_HOST: process.env.SMTP_HOST,
-    SMTP_PORT: process.env.SMTP_PORT,
+    SMTP_PORT: parseInt(process.env.SMTP_PORT ?? ""),
     SMTP_USER: process.env.SMTP_USER,
     SMTP_PASSWORD: process.env.SMTP_PASSWORD,
   },
