3503b85fd7b508ab731d161d0314108b38963959
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-07-29 19:13:35 +0800

    Add DEBUG_MODE as environment variable

--- src/env.ts
+++ src/env.ts
@@ -7,6 +7,7 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    DEBUG_MODE: z.enum(["true", "false"]).transform((v) => v === "true"),
     SECRET_HASH: z.string().trim().min(1),
     STORAGE: z.enum(["local", "online"]).default("local"),
     LOCAL_AVATAR_PATH: z.string().trim().min(1),
@@ -24,6 +25,7 @@ export const env = createEnv({
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     NODE_ENV: process.env.NODE_ENV,
     DATABASE_URL: process.env.DATABASE_URL,
+    DEBUG_MODE: process.env.DEBUG_MODE,
     SECRET_HASH: process.env.SECRET_HASH,
     STORAGE: process.env.STORAGE,
     LOCAL_AVATAR_PATH: process.env.LOCAL_AVATAR_PATH,
