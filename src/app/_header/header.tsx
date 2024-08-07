0261309f5d01e0049e7d04c9c2bc1ecd60485b8f
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-08-08 02:45:55 +0800

    Fix main logo href link

--- src/app/_header/header.tsx
+++ src/app/_header/header.tsx
@@ -23,7 +23,7 @@ export default async function Header() {
     <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
       <nav className="container mx-auto flex h-full max-w-7xl flex-col items-center justify-between gap-2 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link
-          href="#"
+          href={Paths.Home}
           className="flex items-center gap-2 text-lg font-semibold md:text-base"
         >
           <Image
