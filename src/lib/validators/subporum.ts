8a61f72e031fc05b45e7935f8d19c7a0de9795ac
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-08-08 18:19:10 +0800

    Update create subporum schema to filter word

--- src/lib/validators/subporum.ts
+++ src/lib/validators/subporum.ts
@@ -1,11 +1,24 @@
 import { z } from "zod";
 
 export const subporumSchema = z.object({
-  name: z.string().min(3).max(21),
+  name: z
+    .string()
+    .trim()
+    .min(3)
+    .max(21)
+    .refine(
+      (name) => {
+        // avoid word 'create'
+        return name !== "create";
+      },
+      {
+        message: "Subporum name cannot be 'create'",
+      },
+    ),
 });
 
 export const subporumSubscriptionSchema = z.object({
-  subporumId: z.string(),
+  subporumId: z.string().trim(),
 });
 
 export type CreateSubporumInput = z.infer<typeof subporumSchema>;
