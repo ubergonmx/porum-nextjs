de6b484af1704401a35b319279d8c412d0ec13dc
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-08-08 19:10:10 +0800

    Add subporum main page

---
+++ src/app/p/[slug]/page.tsx
@@ -0,0 +1,47 @@
+import { validateRequest } from "@/lib/auth/validate-request";
+import { database as db } from "@/db/database";
+import { notFound } from "next/navigation";
+import { desc } from "drizzle-orm";
+import { subporums } from "@/db/schema";
+import { INFINITE_SCROLL_LIMIT } from "@/lib/constants";
+import MiniCreatePost from "@/components/mini-create-post";
+
+interface PageProps {
+  params: {
+    slug: string;
+  };
+}
+
+const page = async ({ params }: PageProps) => {
+  const { slug } = params;
+
+  const { user } = await validateRequest();
+
+  const subporum = await db.query.subporums.findFirst({
+    where: (table, { eq }) => eq(table.name, slug),
+    orderBy: [desc(subporums.createdAt)],
+    with: {
+      posts: {
+        with: {
+          author: true,
+          comments: true,
+          votes: true,
+          subporum: true,
+        },
+        limit: INFINITE_SCROLL_LIMIT,
+      },
+    },
+  });
+
+  if (!subporum) return notFound();
+
+  return (
+    <>
+      <h1 className="h-14 text-3xl font-bold md:text-4xl">p/{subporum.name}</h1>
+      <MiniCreatePost user={user} />
+      {/* <PostFeed initialPosts={subreddit.posts} subredditName={subreddit.name} /> */}
+    </>
+  );
+};
+
+export default page;
